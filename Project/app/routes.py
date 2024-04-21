323b33351191de91c4d9103144b95c52fe0dec13
Ying Tong <yingtong0106@gmail.com>
2024-04-21 23:35:03 +0800

    added login stuff

--- Project/app/routes.py
+++ Project/app/routes.py
@@ -5,3 +5,7 @@ routes = Blueprint("routes",__name__)
 @routes.route('/home')
 def home():
     return render_template("base.html")
+
+@routes.route("/login")
+def login():
+    return render_template("login.html")
